3b0decb0ecb11f6f0d1a34c2531fbc40a9827c7f
amaranaitsaidi <44408212+amaranaitsaidi@users.noreply.github.com>
2021-05-18 12:14:57 +0200

    Update heatmapsemaine.py
    Changement du fichier de donnée.

--- heatmapsemaine.py
+++ heatmapsemaine.py
@@ -4,23 +4,7 @@ from folium.plugins import HeatMap
 from folium.plugins import HeatMapWithTime
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("./data/humdata.csv", encoding='UTF-8', delimiter='\t').drop(columns = ["utilisateur", "apparenttemperaturemintime",
-                                                                                         "apparenttemperaturemintime", "apparenttemperaturemax",
-                                                                                         "apparenttemperaturemaxtime", "apparenttemperaturehigh",
-                                                                                         "apparenttemperaturehightime", "apparenttemperaturelow",
-                                                                                         "apparenttemperaturelow",
-                                                                                         "apparenttemperaturelowtime",
-                                                                                         "datasource",
-                                                                                         "temperatureoffset2", "cloudcover","cloudcovererror","uvindex",
-                                                                                         "uvindextime","temperaturemintime","temperaturemaxtime","apparenttemperaturemin","windgust",
-                                                                                         "windgusttime","windspeed","windbearing",
-                                                                                         "temperaturemin","temperaturemax",
-                                                                                         "visibility", "geom","precipaccumulation","precipintensitymaxtime",
-                                                                                         "precipprobability","precipintensity","precipintensitymax",
-                                                                                         "temperaturehightime","temperaturelow","temperaturelowtime",
-                                                                                         "dewpoint","summary","icon","sunrisetime","sunsettime","moonphase",
-                                                                                         "timezone","autre_info","Unnamed: 0","pressure","village","environnement_precision","date_server","age","precision_geo"])
-
+df = pd.read_csv("./data/humdata_sort.csv", encoding='UTF-8', delimiter=',')
 
 
 
@@ -28,21 +12,9 @@ loc = 'Carte de chaleur (Heat Map) des signalements des tiques sur la région du
 title_html = '''
                  <h6 align="center" style="font-size:16px"><b>{}</b></h6>
                  '''.format(loc)
-df.time = pd.to_datetime(df.time, format='%Y-%m-%d %H:%M:%S')
-df['year'] = df.time.apply(lambda x: x.year)
-df['month'] = df.time.apply(lambda x: x.month)
-df['week'] = df.time.apply(lambda x: x.week)
-df['day'] = df.time.apply(lambda x: x.day)
-df['hour'] = df.time.apply(lambda x: x.hour)
-df["moisannee"] = df.time.map(lambda x: x.strftime('%Y-%m'))
+
 
 df_copy = df.copy()
-
-
-
-print(plt.hist(df_copy.month))
-
-print(df_copy.head(5))
 
 df_copy["count"] = 1
 
@@ -60,8 +32,8 @@ df_copy['count'] = 1
 base_map = generateBaseMap()
 HeatMap(data=df_copy[['lat', 'lon', 'count']].groupby(['lat', 'lon']).sum().reset_index().values.tolist(), radius=8).add_to(base_map)
 df_hour_list = []
-for moisannee in df_copy.moisannee.sort_values().unique():
-    df_hour_list.append(df_copy.loc[df_copy.moisannee == moisannee, ['lat', 'lon', 'count']].groupby(['lat', 'lon']).sum().reset_index().values.tolist())
+for week in df_copy.week.sort_values().unique():
+    df_hour_list.append(df_copy.loc[df_copy.week == week, ['lat', 'lon', 'count']].groupby(['lat', 'lon']).sum().reset_index().values.tolist())
 
 base_map = generateBaseMap(default_zoom_start=7)
 HeatMapWithTime(df_hour_list,min_speed= 3 ,radius=5, gradient={0.2: 'blue' , 0.4: 'lime', 0.6: 'orange', 1: 'red'}, min_opacity=0.5, max_opacity=0.8, use_local_extrema=True).add_to(base_map)
